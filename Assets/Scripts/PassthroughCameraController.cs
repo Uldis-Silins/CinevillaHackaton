da1af53196536679f7ba51f2db6cba0e7f5dd08b
Uldis Šiliņš <dissid@inbox.lv>
2025-06-14 02:10:34 +0300

    particle flipbook

--- Assets/Scripts/PassthroughCameraController.cs
+++ Assets/Scripts/PassthroughCameraController.cs
@@ -5,8 +5,17 @@ public class PassthroughCameraController : MonoBehaviour
 {
     [SerializeField] private OVRPassthroughLayer m_passthroughLayer;
 
+    [SerializeField] private AnimationCurve m_fireCurve;
+
+    private bool m_inFireAnimation;
+
     private void Update()
     {
         m_passthroughLayer.SetBrightnessContrastSaturation(0f, 0f, 1f - Mathf.PingPong(Time.time, 2f));
+    }
+
+    public void AnimateFire()
+    {
+        m_inFireAnimation = true;
     }
 }
